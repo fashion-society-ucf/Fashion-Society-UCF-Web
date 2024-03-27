7374e4abe231403d826cec53aeb6b1cc70835a99
helixDevelopment <helix.dev.xyz@gmail.com>
2024-03-27 08:58:23 -0400

    Navbar moved down and refactored

--- fashionsociety/src/components/ui/NavBar.js
+++ fashionsociety/src/components/ui/NavBar.js
@@ -2,21 +2,23 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom"
 import "./NavBar.css"
 
-export default function NavBar () {
+export default function NavBar() {
     const location = useLocation();
 
     console.log(location.pathname);
 
-    return(
-        <nav className="navbar">
-            <div className="links">
-                <ul>
-                    <li className={location.pathname === "/" ? "current" : ""}><Link to="/">Home</Link></li>
-                    <li className={location.pathname === "/about" ? "current" : ""}><Link to="/about">About</Link></li>
-                    <li className={location.pathname === "/committees" ? "current" : ""}><Link to="/committees">Committees</Link></li>
-                    <li className={location.pathname === "/events" ? "current" : ""}><Link to="/events">Events</Link></li>
-                    <div class="animation start-home"></div>
-                </ul>
+    return (
+        <nav>
+            <div className="navbar">
+                <div className="links">
+                    <ul>
+                        <li className={location.pathname === "/" ? "current" : ""}><Link to="/">Home</Link></li>
+                        <li className={location.pathname === "/about" ? "current" : ""}><Link to="/about">About</Link></li>
+                        <li className={location.pathname === "/committees" ? "current" : ""}><Link to="/committees">Committees</Link></li>
+                        <li className={location.pathname === "/events" ? "current" : ""}><Link to="/events">Events</Link></li>
+                        <div className="animation start-home"></div>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
