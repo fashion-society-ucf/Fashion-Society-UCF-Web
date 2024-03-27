068d56d1021a282a3721bb4c0765230fd0b6f9ad
helixDevelopment <helix.dev.xyz@gmail.com>
2024-03-27 17:46:28 -0400

    ugly card popout code

--- fashionsociety/src/components/pages/Comittees/Committees.js
+++ fashionsociety/src/components/pages/Comittees/Committees.js
@@ -1,49 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { memo, useState } from "react";
 
 import "./committees.css";
 
+const CommitteeCard = memo(({ name, image, selected, select }) => {
+    return (
+        <div className={`item ${selected === name ? "selected" : ""}`} onClick={select}>
+            <div className="text">
+                <p className="name">
+                    {name}
+                    <p className="description">
+                        Design is a committee made for UCF’s best fashion designers and aspirational beginners alike. Whether it’s through sewing, knitting, crocheting, or upcycling, this committee is perfect for creative individuals who want to develop their talents and help design outfits for our spring runway show! Open to all skill levels, we are here to help students explore and learn fashion design, as well as hone in on their craft to fabricate runway-ready looks.
+                    </p>
+                </p>
+            </div>
+            <img src={image} alt={name} />
+        </div>
+    )
+});
+
 export default function Committees() {
+    const [selected, setSelected] = useState("");
+
+    const select = (name) => {
+        if (selected === name) {
+            setSelected("");
+            return;
+        }
+        setSelected(name);
+    };
+
     return (
         <div className="content">
             <div className="list-container">
                 <div className="list">
-                    <Link className="item" to="/design">
-                        <p>Design</p>
-                        <img src="/committees/design.png" alt="Design" />
-                    </Link>
-                    <Link className="item" to="/styling">
-                        <p>Styling</p>
-                        <img src="/committees/styling.png" alt="Styling" />
-                    </Link>
-                    <Link className="item" to="/modeling">
-                        <p>Modeling</p>
-                        <img src="/committees/modeling.png" alt="Modeling" />
-                    </Link>
-                    <Link className="item" to="/beauty">
-                        <p>Beauty</p>
-                        <img src="/committees/beauty.png" alt="Beauty" />
-                    </Link>
-                    <Link className="item" to="/photography">
-                        <p>Photography</p>
-                        <img src="/committees/photography.png" alt="Photography" />
-                    </Link>
-                    <Link className="item" to="/socialmedia">
-                        <p>Social Media</p>
-                        <img src="/committees/socialmedia.png" alt="Social Media" />
-                    </Link>
-                    <Link className="item" to="/production">
-                        <p>Production</p>
-                        <img src="/committees/production.png" alt="Production" />
-                    </Link>
-                    <Link className="item" to="/webdesign">
-                        <p>Web Design</p>
-                        <img src="/committees/webdesign.webp" alt="Web Design" />
-                    </Link>
-                    <Link className="item" to="/zine">
-                        <p>Zine</p>
-                        <img src="/committees/zine.png" alt="Zine" />
-                    </Link>
+                    <CommitteeCard name="Design" image="/committees/design.png" selected={selected} select={() => select("Design")} />
+                    <CommitteeCard name="Styling" image="/committees/styling.png" selected={selected} select={() => select("Styling")} />
+                    <CommitteeCard name="Modeling" image="/committees/modeling.png" selected={selected} select={() => select("Modeling")} />
+                    <CommitteeCard name="Beauty" image="/committees/beauty.png" selected={selected} select={() => select("Beauty")} />
+                    <CommitteeCard name="Photography" image="/committees/photography.png" selected={selected} select={() => select("Photography")} />
+                    <CommitteeCard name="Social Media" image="/committees/socialmedia.png" selected={selected} select={() => select("Social Media")} />
+                    <CommitteeCard name="Production" image="/committees/production.png" selected={selected} select={() => select("Production")} />
+                    <CommitteeCard name="Web Design" image="/committees/webdesign.png" selected={selected} select={() => select("Web Design")} />
+                    <CommitteeCard name="Zine" image="/committees/zine.png" selected={selected} select={() => select("Zine")} />
                 </div>
             </div>
         </div>
