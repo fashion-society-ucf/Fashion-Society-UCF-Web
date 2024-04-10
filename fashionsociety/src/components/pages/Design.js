842b192e4accb853c2071c7d11e3e21dcf2b4187
sophia <so637054@ucf.edu>
2024-04-10 00:00:21 -0400

    chairmen section

--- fashionsociety/src/components/pages/Design.js
+++ fashionsociety/src/components/pages/Design.js
@@ -2,17 +2,37 @@ import React from 'react';
 import './design-modeling.css';
 
 export default function Design () {
-  const bannerUrl = process.env.PUBLIC_URL + '/banners/design.png';
+  const publicUrl = process.env.PUBLIC_URL;
+  const bannerUrl = publicUrl + '/banners/design.png';
+  const kylieUrl = publicUrl + '/eboard photos/kylie-headshot.JPG';
+  const samaraUrl = publicUrl + '/eboard photos/samara-headshot.JPG';
+  const christianUrl = publicUrl + '/eboard photos/christian-headshot.JPG';
 
   return(
     <div>
       <div className="banner" style={{backgroundImage: `url(${bannerUrl})`}}>
-           <h1 className="design">Design</h1>
+        <h1 className="design">Design</h1>
       </div>
 
-      {/* <div className="chairmen">
-           <h1 className="underlined">Design Chairmen</h1>
-      </div> */}
+      <div className="underlinedText">
+        <span>Design Chairmen</span>
+      </div>
+
+      <div className="chairmanImage">
+        <div>
+          <img src={kylieUrl} alt="Kylie Knickerbocker" />
+          <div className="subtitle">Kylie Knickerbocker</div>
+        </div>
+        <div>
+          <img src={samaraUrl} alt="Samara Dyal" />
+          <div className="subtitle">Samara Dyal</div>
+        </div>
+        <div>
+          <img src={christianUrl} alt="Christian Perry" />
+          <div className="subtitle">Christian Perry</div>
+        </div>
+      </div>
+
     </div>
   )
 }
--- fashionsociety/src/components/pages/Modeling.js
+++ fashionsociety/src/components/pages/Modeling.js
@@ -2,11 +2,37 @@ import React from 'react';
 import './design-modeling.css';
 
 export default function Modeling () {
-  const bannerUrl = process.env.PUBLIC_URL + '/banners/modeling.png';
+  const publicUrl = process.env.PUBLIC_URL;
+  const bannerUrl = publicUrl + '/banners/modeling.png';
+  const eleonoraUrl = publicUrl + '/eboard photos/eleonora-headshot.JPG';
+  const robinUrl = publicUrl + '/eboard photos/robin-headshot.JPG';
+  const avalonUrl = publicUrl + '/eboard photos/avalon-headshot.JPG';
 
   return(
+    <div>
       <div className="banner" style={{backgroundImage: `url(${bannerUrl})`}}>
            <h1 className="modeling">Modeling</h1>
       </div>
+
+      <div className="underlinedText">
+        <span>Modeling Chairmen</span>
+      </div>
+
+      <div className="chairmanImage">
+        <div>
+          <img src={eleonoraUrl} alt="Eleonora Yordanova" />
+          <div className="subtitle">Eleonora Yordanova</div>
+        </div>
+        <div>
+          <img src={robinUrl} alt="Robin Thomas" />
+          <div className="subtitle">Robin Thomas</div>
+        </div>
+        <div>
+          <img src={avalonUrl} alt="Avalon Ramsey" />
+          <div className="subtitle">Avalon Ramsey</div>
+        </div>
+      </div>
+      
+    </div>
   )
 }
